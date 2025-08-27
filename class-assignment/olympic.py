1bdd3683a2158aebb1277bb39ba567e6176a9cda
Minoo-Yh <minoo.yaghoubi@gmail.com>
2025-08-27 13:51:54 +0300

    class excersices

---
+++ class-assignment/olympic.py
@@ -0,0 +1,21 @@
+Olampic_Year = int(input("Enter the Olampic Year:"))
+
+while year >=1896:
+    year = int(input("Enter the year:"))
+
+
+    if year % 4 ==0:
+      print("It was on olampic years ")
+
+    else:
+        print("NO")
+        year = int(input("Enter the year:"))
+
+    print( "" )
+
+
+
+
+
+
+
